6eb625f1f26992c479a3d4af7fb6ad6fd3914f0b
ITJoker233 <649258008@qq.com>
2020-02-03 14:40:21 +0800

    🎉 Update Version 1.6.5

--- assets/media/script/weather.js
+++ assets/media/script/weather.js
@@ -11,7 +11,7 @@
     c.rel = 'stylesheet'
     c.href = 'https://apip.weatherdt.com/float/static/css/tqw_widget_float.css?v=0101'
     var s = d.createElement('script')
-    s.src = 'https://cdn.jsdelivr.net/gh/itjoker233/Gridea-theme-Chic@latest/assets/media/script/tqw_widget_float.min.js?v=0101'
+    s.src = 'https://cdn.jsdelivr.net/gh/itjoker233/Gridea-theme-Chic@1.6.5/assets/media/script/tqw_widget_float.min.js?v=0101'
     var sn = d.getElementsByTagName('script')[0]
     sn.parentNode.insertBefore(c, sn)
     sn.parentNode.insertBefore(s, sn)
